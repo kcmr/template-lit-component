dc33c023432cca0bdbee6bd104f006ba4c214162
Kus Cámara <kcmr@users.noreply.github.com>
2019-12-07 20:52:20 +0100

    reset fixtures in tests

--- test/{{component}}.test.js
+++ test/{{component}}.test.js
@@ -2,6 +2,10 @@ import { fixture, fixtureCleanup, html, assert } from '@open-wc/testing';
 import '../{{component}}.js';
 
 suite('<{{component}}>', () => {
+  teardown(() => {
+    fixtureCleanup();
+  });
+
   test('"myProperty" can be set via "my-property" attribute', async() => {
     const el = await fixture(html`
       <{{component}} my-property="foo"></{{component}}>
